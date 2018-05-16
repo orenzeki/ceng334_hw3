372b4148634f3b257447b329ec6921d25f00cd06
Yagmur Oymak <yagmur.oymak@gmail.com>
2018-05-17 02:08:26 +0300

    Helpers to read blocks etc

--- src/main.cpp
+++ src/main.cpp
@@ -19,9 +19,17 @@ class Image {
 public:
     Image(const char *path);
     ~Image();
+
+    uint8_t *get_block(unsigned int block) const;
+    struct ext2_super_block *get_super_block() const;
+    struct ext2_group_desc *get_group_desc() const;
 private:
     uint8_t *image;
+    uint8_t *first_block;
     size_t len;
+    unsigned int block_size;
+    struct ext2_super_block *super;
+    struct ext2_group_desc *group;
 };
 
 Image::Image(const char *path)
@@ -38,16 +46,21 @@ Image::Image(const char *path)
         perror("open");
         throw std::runtime_error("Cannot open file");
     }
-    /*
-     * TODO: Decide if we will map it at 0 and use base_offset in
-     * later references, or map with base_offset and be happy later.
-     */
     image = (uint8_t *) mmap(NULL, len, PROT_READ | PROT_WRITE,
             MAP_SHARED, fd, 0);
     if (image == (void *) -1) {
         perror("mmap");
         throw std::runtime_error("Cannot map file");
     }
+
+    first_block = image + base_offset;
+    super = (struct ext2_super_block *) first_block;
+    if (super->s_magic != EXT2_SUPER_MAGIC) {
+        fprintf(stderr, "Not an ext2 fs\n");
+        throw std::runtime_error("Invalid filesystem");
+    }
+    block_size = 1024 << super->s_log_block_size;
+    group = (struct ext2_group_desc *) (first_block + block_size);
 
     close(fd);
 }
@@ -57,6 +70,21 @@ Image::~Image()
     munmap(image, len);
 }
 
+uint8_t *Image::get_block(unsigned int block) const
+{
+    return first_block + (block - 1)*block_size;
+}
+
+struct ext2_super_block *Image::get_super_block() const
+{
+    return super;
+}
+
+struct ext2_group_desc *Image::get_group_desc() const
+{
+    return group;
+}
+
 int main(int argc, char **argv)
 {
     if (argc != 2) {
@@ -64,6 +92,11 @@ int main(int argc, char **argv)
         return 1;
     }
     Image img(argv[1]);
+
+    struct ext2_super_block *super = img.get_super_block();
+    std::cout << super->s_blocks_count << '\n';
+    std::cout << super->s_first_ino << '\n';
+
 
     return 0;
 }
